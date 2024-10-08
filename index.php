4278fffba89aaf2ce844a9441bc44a5e0fd07e03
1u1ucas <lucasleveque17@gmail.com>
2024-10-08 13:30:41 +0200

    --

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-class Order {private int $id;
+class Order {
+
+    public static $CART_STATUS = "CART";
+	public static $SHIPPING_ADDRESS_SET_STATUS = "SHIPPING_ADDRESS_SET";
+	public static $SHIPPING_METHOD_SET_STATUS = "SHIPPING_METHOD_SET";
+	public static $PAID_STATUS = "PAID";
+	public static $MAX_PRODUCTS_BY_ORDER = 5;
+	public static $BLACKLISTED_CUSTOMERS = ['David Robert'];
+	public static $UNIQUE_PRODUCT_PRICE = 5;
+	public static $AUTORIZED_SHIPPING_COUNTRIES = ['France', 'Belgique', 'Luxembourg'];
+	public static $AVAILABLE_SHIPPING_METHODS = ['Chronopost Express', 'Point relais', 'Domicile'];
+	public static $PAID_SHIPPING_METHOD = 'Chronopost Express';
+
+	public static $PAID_SHIPPING_METHODS_COST = 5;
+    
+    private int $id;
 
     private array $products;
 
@@ -24,20 +39,21 @@ class Order {private int $id;
         $this->totalPrice = 5*count($products);
         $this->id = rand();
 
-        if ($this->customerName === 'David Robert') {
-            throw new Exception('David Robert n\'a pas le droit de commander');
+        if ($this->customerName === Order::$BLACKLISTED_CUSTOMERS){
+            throw new Exception ( Order::$BLACKLISTED_CUSTOMERS .' n\'a pas le droit de commander');
         }
-        if (count($this->products) > 5) {
-            throw new Exception('Vous ne pouvez pas commander plus de 5 produits');
-        }
+
+        if (count($products) > Order::$MAX_PRODUCTS_BY_ORDER) {
+			throw new Exception("Vous ne pouvez pas commander plus de " . Order::$MAX_PRODUCTS_BY_ORDER . " produits");
+		}
 
         echo "Commande {$this->id} passée !";
     }
 
     public function deleteProduct($product) {
 
-        if ($this->status !== 'CART') {
-            throw new Exception('Vous ne pouvez pas supprimer de produit d\'une commande payée');
+        if ($this->status !== Order::$CART_STATUS) {
+            throw new Exception('Vous ne pouvez pas supprimer de produit d\'une commande qui n\'est pas en cours');
         }
 
         if (($key = array_search($product, $this->products)) !== false) {
@@ -49,18 +65,18 @@ class Order {private int $id;
 
     public function addProduct($product) {
 
-        if (!$this->status === 'CART') {
-            throw new Exception('Vous ne pouvez pas ajouter de produit à une commande payée');
+        if (!$this->status === Order::$CART_STATUS) {
+            throw new Exception('Vous ne pouvez pas ajouter de produit à une commande ');
         }
 
-        if (count($this->products) >= 5) {
-            throw new Exception('Vous ne pouvez pas commander plus de 5 produits');
+        if (count($this->products) >= Order::$MAX_PRODUCTS_BY_ORDER) {
+            throw new Exception('Vous ne pouvez pas commander plus de ' . Order::$MAX_PRODUCTS_BY_ORDER .' produits');
         }
         if (in_array($product, $this->products)) {
             throw new Exception('Vous avez déjà commandé ce produit');
         }
         $this->products[] = $product;
-        $this->totalPrice += 5*count($this->products);
+        $this->totalPrice += Order::$MAX_PRODUCTS_BY_ORDER*count($this->products);
 
         return 'Produit ajouté';
 
@@ -68,44 +84,47 @@ class Order {private int $id;
 
     public function adressShipping($shippingAddress) {
 
-        if ($this->status !== 'CART') {
-            throw new Exception('Vous ne pouvez pas modifier une commande payée');
+        if ($this->status !== Order::$CART_STATUS) {
+            throw new Exception('Vous ne pouvez pas modifier une commande qui n\'est pas en cours');
         }
 
-        if (!in_array($shippingAddress, ['France', 'Belgique', 'Luxembourg'])) {
+        if (!in_array($shippingAddress, Order::$AUTORIZED_SHIPPING_COUNTRIES)) {
             throw new Exception('Nous ne livrons pas dans ce pays');
         }
 
         $this->shippingAddress = $shippingAddress;
+        $this->status = Order::$SHIPPING_ADDRESS_SET_STATUS;
+
         return 'Adresse de livraison ajoutée';
 
     }
 
     public function pay($shippingMethod) {
 
-        if ($this->status !== 'CART') {
-            throw new Exception('Commande déjà payée');
+        if ($this->status !== Order::$SHIPPING_ADDRESS_SET_STATUS) {
+            throw new Exception('Commande non conforme');
         }
 
         if ($this->shippingAddress === null) {
             throw new Exception('Vous devez renseigner une adresse de livraison');
         }
 
-        if (!in_array($shippingMethod, ['ChronoPost', 'Express', 'Domicille', 'Point-Relais'])) {
+        if (!in_array($shippingMethod, Order::$AVAILABLE_SHIPPING_METHODS)) {
             throw new Exception('Méthode de livraison non reconnue');
         }
 
-        if ($this->$shippingMethod === 'Express'){
-            $this->totalPrice += 5;
+        if ($this->$shippingMethod === Order::$PAID_SHIPPING_METHOD) {
+            $this->totalPrice += Order::$PAID_SHIPPING_METHODS_COST;
         }
 
         $this->shippingMethod = $shippingMethod;
+        $this->status = Order::$SHIPPING_METHOD_SET_STATUS;
         return 'Méthode de livraison choisie';
     }
 
     public function buyOrder () {
-        if ($this->status !== 'CART') {
-            throw new Exception('Commande déjà payée');
+        if ($this->status !== Order::$SHIPPING_METHOD_SET_STATUS) {
+            throw new Exception('Commande non conforme');
         }
 
         if ($this->shippingAddress === null) {
@@ -116,7 +135,7 @@ class Order {private int $id;
             throw new Exception('Vous devez renseigner une méthode de livraison');
         }
 
-        $this->status = 'PAID';
+        $this->status = Order::$PAID_STATUS;
         return 'Commande payée';
     }
 
