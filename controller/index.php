c9aa6f185799374f753cba14593f386dffbf5ecb
1u1ucas <lucasleveque17@gmail.com>
2024-10-08 15:26:55 +0200

    --

---
+++ controller/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../model/Order.php';
+
+
+try {
+    $order = new Order('john Doe', ['iphone', 'macbook', 'ipad']);
+
+	require_once '../view/order-created.php';
+
+} catch (Exception $e) {
+
+	require_once '../view/order-error.php';
+}
